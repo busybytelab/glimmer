bf453d20482fd5b71c7370a65f5308228dbec690
mo.asgari <masgari@users.noreply.github.com>
2025-06-08 08:12:02 +0100

    Refactor markdown styles and PostCSS configuration: Move markdown styles to global CSS, update PostCSS plugin configuration, simplify Svelte config

--- ui/postcss.config.cjs
+++ ui/postcss.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-  plugins: {
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 } 
